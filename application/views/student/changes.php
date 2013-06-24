509bb8fa2b3138108e657774153ee2990909d611
Manos Nikiforakis <nikiforakis.m@gmail.com>
2013-06-24 23:58:46 +0300

    start adding functionality to changes view

--- application/views/student/changes.php
+++ application/views/student/changes.php
@@ -1,7 +1,275 @@
 <script type="text/javascript">
 
-//$(document).ready(function(){
-//});
+function my_curr_date() {      
+    var currentDate = new Date()
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
+    var my_date = day+"-"+month+"-"+year;
+    return my_date;
+};
+
+
+var monthnames={1:'Ιανουαρίου',
+               2:'Φεβρουαρίου',
+               3:'Μαρτίου',
+               4:'Απριλίου',
+               5:'Μαΐου',
+               6:'Ιουνίου',
+               7:'Ιουλίου',
+               8:'Αυγούστου',
+               9:'Σεπτεμβρίου',
+               10:'Οκτωβρίου',
+               11:'Νοεμβρίου',
+               12:'Δεκεμβρίου'};
+
+var firstchange = false; 
+
+$(document).ready(function() {
+  var newindex = 0;
+  var newchange = 0;
+  var undobtn = document.getElementById('undo_change');
+  undobtn.disabled=true;  
+
+
+  $('#firstchangebtn').click(function(){
+      $('#nochanges').hide();
+      $('#startchanges').removeClass("hidden");
+      $('#actions').removeClass("hidden");
+      firstchange = true;
+      undobtn.disabled=false;         
+      newchange = newchange + 1;
+      set_latest_apyno(newchange);
+      set_pay_data(newchange);
+      $('#apydate1').attr('value', my_curr_date());
+      var $divs = $('#startchanges form > fieldset > legend').siblings();
+      $divs.show();
+  });
+  
+
+
+  $('#add_change').click(function(){
+          undobtn.disabled=false; 
+          newchange = newchange + 1;
+          newindex = - newchange;
+          var lastfieldset = $(this).parents('form').find('fieldset:last');
+          var newfieldset = lastfieldset.clone();
+          newfieldset.insertAfter(lastfieldset);
+          var fields = newfieldset.find('input[type="text"]');
+          
+          //-------------set new apyno---------------
+          // fields.eq(0).attr("name", "apy_no[" + newindex +"]");        
+          // fields.eq(0).attr('id', "apyno"+newchange);
+
+          // if (firstchange==false && newchange==1){
+          //   set_latest_apyno(newchange);
+          // }
+          // else{
+          //   var newapyno = parseInt(fields.eq(0).val(), 10)+1;
+          //   fields.eq(0).prop('value', newapyno);
+          //   fields.eq(0).attr('value', newapyno);  
+          // };
+
+
+          //-------------set new date---------------
+          //I use attr to set the new value because the input is "dirty". If I use val() then 
+          //it changes the visible value but not the value property of the field!!!
+          fields.eq(0).attr("name", "change_dt[" + newindex +"]");  
+          if (newchange==1 && firstchange==false){
+            fields.eq(0).attr('value', my_curr_date());
+          };      
+          
+          //----------set new month price----------
+          fields.eq(2).attr("name", "new_month_price[" + newindex +"]");  
+
+
+          //----------set new month num-------------
+          // var prevmonth=parseInt(fields.eq(3).val(),10);
+          // var newmonth;
+          // if(prevmonth==12){
+          //   newmonth=1;  
+          // } 
+          // else {
+          //   newmonth=parseInt(fields.eq(3).val(),10)+1;  
+          // };
+          
+          newfieldset.find('legend').empty();
+          newfieldset.find('legend').append('<span><i class="icon-certificate"></i></span>');
+          newfieldset.find('legend').append('<div class="legend-text"> Μεταβολή ' + my_curr_date() +'</div>');
+          if (firstchange==false) {
+              newfieldset.find('legend').append('<div class="legend-selector"> <input class="pull-right" type="checkbox" name="select['+ newindex +'] value=\'0\'"></div>');
+          };
+          
+          // fields.eq(3).prop('value', newmonth);  
+          // fields.eq(3).attr('value', newmonth);  
+          
+          // fields.eq(3).attr("name", "month_range[" + newindex +"]");
+
+          //-----------set new reason textarea-------------
+          var txtareafields = newfieldset.find('input[type="textarea"]');
+          txtareafields.eq(0).attr("name", "reason[" + newindex +"]");  
+
+          //-----------set new notes textarea-------------
+          var txtareafields = newfieldset.find('input[type="textarea"]');
+          txtareafields.eq(1).attr("name", "notes[" + newindex +"]");  
+
+          //----------set new is_credit checkbox----------
+          // var chkboxfields = newfieldset.find('input[type="checkbox"]');
+      
+          // chkboxfields.eq(2).attr("name", "is_credit[" + newindex +"]");  
+          // chkboxfields.eq(2).removeAttr("checked");
+          // chkboxfields.eq(2).val(0);  
+
+          //if there are no previous payments the select boxes have no meaning!
+          if (firstchange==false){
+              //----------set new select checkbox----------
+              //normal view
+              chkboxfields.eq(1).attr("name", "select[" + newindex +"]");  
+              chkboxfields.eq(1).removeAttr("checked");
+              chkboxfields.eq(1).val(0);
+              chkboxfields.eq(1).prop('disabled', true);
+              //responsive view
+              chkboxfields.eq(0).attr("name", "select[" + newindex +"]");  
+              chkboxfields.eq(0).removeAttr("checked");
+              chkboxfields.eq(0).val(0); 
+              chkboxfields.eq(0).prop('disabled', true);
+          };
+
+
+          //----------disable the buttons-------------
+          newfieldset.find('a.btn').attr('onclick', 'return false;').addClass('disabled');
+
+          //----------the new payments should always start visible!------
+          var $divs = newfieldset.find('legend').siblings();
+          $divs.show();
+          //newfieldset.find('legend > span').html('<i class="icon-certificate"></i>');
+  });
+
+
+    $('.delbtn').tooltip({
+      title:"Διαγραφή ΑΠΥ",
+      trigger:'hover',
+      placement: 'top',
+      container:'body'
+   });
+
+
+  $('#undo_change').click(function(){
+    if (newchange > 0) {
+      var lastfieldset = $(this).parents('form').find('fieldset:last');
+
+      if (firstchange==false || (firstchange==true && newchange>1)){
+        lastfieldset.remove();  
+        newchange = newchange - 1;
+      }
+      else if(newchange==1){
+      //I don't remove the last fieldset because then there would be no fieldset to clone!!!     
+        newchange = newchange - 1;
+      };
+      
+      if (newchange==0){
+        var fieldsets = $(this).parents('fieldset').length;   
+        undobtn.setAttribute('disabled','disabled'); 
+        if (firstchange==true){
+          $('#startchanges').addClass("hidden");
+          $('#actions').addClass("hidden");
+          $('#nochanges').show();
+        }
+        //if I select all payments to be erased or canceled and I already have inserted a new record
+        //then the new record will stay in place BUT if one presses undo, then there will be no payment
+        //and no fieldset to clone so we need a new page load from the server
+        else if (fieldsets==0) {
+          window.location.href=window.location.href;
+        }
+      }
+    }
+  });
+
+  //when we click a select box in responsive view the corresponding hidden select box in the normal view should get the same value.
+  $('body').on('click', '.legend-selector input[type="checkbox"]', function(){
+      var alterchkbox = $(this).parents('fieldset').find('.selector:hidden input[type="checkbox"]');
+      if($(this).val()==0){
+        $(this).val(1);
+        $(this).attr('checked', 'checked');
+        alterchkbox.prop('checked', true); //makes the checkbox checked
+        alterchkbox.attr('checked', 'checked'); //just adds the property checked but it doesn't check the chekbox!!!
+        alterchkbox.val(1);
+      }
+      else
+      {
+        $(this).val(0); 
+        $(this).removeAttr('checked');
+        alterchkbox.val(0);
+        alterchkbox.removeAttr('checked');
+      }
+  });
+
+  //when we click a select box in normal view the corresponding hidden select box in the responsive view should get the same value.
+  $('body').on('click', '.selector input[type="checkbox"]', function(){
+      var alterchkbox = $(this).parents('fieldset').find('.legend-selector input[type="checkbox"]');
+      if($(this).val()==0){
+        $(this).val(1);
+        $(this).attr('checked', 'checked');
+        alterchkbox.prop('checked', true); //makes the checkbox checked
+        alterchkbox.attr('checked', 'checked'); //just adds the property checked but it doesn't check the chekbox!!!
+        alterchkbox.val(1);
+      }
+      else
+      {
+        $(this).val(0); 
+        $(this).removeAttr('checked');
+        alterchkbox.val(0);
+        alterchkbox.removeAttr('checked');
+      }
+  });
+
+
+//delete or cancel multiple payments using the select checkboxes and the combobox below (the action fires through ajax)
+  $('#select_action').change(function(){
+      var act=$(this).val();
+      var fieldsets = $('form').find('fieldset');
+      var selected_chkboxes = $('form').find(':input[type="checkbox"][name*="select"]:visible:checked');
+      var allselected = false;
+      if(selected_chkboxes.length == fieldsets.length) allselected = true;
+      if (act!='none' && selected_chkboxes.length>0) {
+        var selected_chkboxes = $('form').find(':input[type="checkbox"][name*="select"]:visible:checked');
+        var sData = selected_chkboxes.serialize();
+        switch(act){
+          case 'delete':
+            var msg="Πρόκειται να διαγράψετε τις επιλεγμένες πληρωμές. Η ενέργεια αυτή δεν αναιρείται. Παρακαλώ επιβεβαιώστε.";
+            var post_url = "<?php echo base_url();?>student/payment_batch_actions/delete";
+            break;
+          case 'cancel':
+            var msg="Πρόκειται να ακυρώσετε τις επιλεγμένες πληρωμές. Η ενέργεια αυτή δεν αναιρείται. Παρακαλώ επιβεβαιώστε.";
+            var post_url = "<?php echo base_url();?>student/payment_batch_actions/cancel";
+            break;
+        };
+        var ans=confirm(msg);
+        if (ans==true){
+            $.ajax({
+              type: "post",
+              url: post_url,
+              data : sData,
+              dataType:'json', 
+              success: function(){
+                if (allselected==true && newchange==0){
+                    window.location.href = window.location.href;  
+                    //window.location.reload(true); it pops up an alert message from the browser
+                }
+              }
+            }); //end of ajax
+            selected_chkboxes.each(function(){
+              $(this).parents('fieldset').remove();
+            });
+            $(this).prop('selectedIndex',0);
+        } //end if ans
+      } //end if act
+  })
+
+
+
+
+});
 
 </script>
 
@@ -146,7 +414,7 @@
                   </div>
                
                   <div class="span2">
-                    <input type="text" class="span12" name="reason[<?php echo $data['id'];?>]" value="<?php echo $data['reason'];?>">
+                    <input type="textarea" class="span12" name="reason[<?php echo $data['id'];?>]" value="<?php echo $data['reason'];?>">
                   </div>
 
                   <div class="span2">
@@ -188,7 +456,7 @@
                   <p>Δεν υπάρχει καμία μεταβολή στα δίδακτρα!</p>
                 </div>
                 <div style="margin-bottom:70px;">
-                  <button type="button" class="btn btn-primary pull-right" id="firstchange">Μεταβολή</button>
+                  <button type="button" class="btn btn-primary pull-right" id="firstchangebtn">Μεταβολή</button>
                 </div>
               </div>
 
@@ -223,11 +491,11 @@
                             </div>
 
                             <div class="span2">
-                              <input type="text" id="reason1" class="span12" name="reason[-1]" value="">
+                              <input type="textarea" id="reason1" class="span12" name="reason[-1]" value="">
                             </div>
 
                             <div class="span2">
-                              <input type="text" id="notes1" class="span12" name="notes[-1]" value="">
+                              <input type="textarea" id="notes1" class="span12" name="notes[-1]" value="">
                             </div>
 
                             <div class="span2">
@@ -241,8 +509,8 @@
                 </fieldset> <!--end of fieldset-->
 
                 <div id="actions" class="form-actions hidden">
-                  <button type="button" class="btn btn-primary pull-right" name="add_payment" id="add_payment">Πληρωμή</button>
-                  <button type="button" class="btn" name="undo_payment" id="undo_payment">Αναίρεση</button>
+                  <button type="button" class="btn btn-primary pull-right" name="add_change" id="add_change">Πληρωμή</button>
+                  <button type="button" class="btn" name="undo_change" id="undo_change">Αναίρεση</button>
                   <button type="submit" class="btn btn-danger" id="submit1" name="submit">Αποθήκευση</button>
                 </div>
 
